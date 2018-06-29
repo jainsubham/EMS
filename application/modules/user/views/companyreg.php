73392b4b87353fd2a0580497409d359b77ab930b
Your Name <shubhamjainpa@gmail.com>
2018-06-29 10:16:52 +0530

    Registration page of company

--- application/modules/user/views/companyreg.php
+++ application/modules/user/views/companyreg.php
@@ -1,75 +1,93 @@
+
+<?=  link_tag('assets/css/font-awesome.min.css'); ?>
+<!-- 
+<style type="text/css">
+    body{
+        background:url('<?=  base_url('assets/img/bg.jpg'); ?>') no-repeat 0% 87% fixed;
+    }
+</style> -->
+<?= link_tag('assets/css/style.css'); ?>
+<link href='//fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'><link href='//fonts.googleapis.com/css?family=Raleway+Dots' rel='stylesheet' type='text/css'>
 <? include "header.php"; ?>
 
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+<!-- <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+ //function hideURLbar(){ window.scrollTo(0,1); } </script> -->
+<!-- Meta tag Keywords -->
+<!-- css files -->
 
 
-<div class="container">
-	<div class="row">
-        <div class="col-md-6">
-            <form action="" method="post" id="fileForm" role="form">
-            <fieldset><legend class="text-center">Valid information is required to register. <span class="req"><small> required *</small></span></legend>
+<!-- //css files -->
+<!-- online-fonts -->
 
-            <div class="form-group">
-            <label for="phonenumber"><span class="req">* </span> Phone Number: </label>
-                    <input required type="text" name="phonenumber" id="phone" class="form-control phone" maxlength="28" onkeyup="validatephone(this);" placeholder="not used for marketing"/> 
-            </div>
+</head>
+<body>
 
-            <div class="form-group"> 	 
-                <label for="firstname"><span class="req">* </span> First name: </label>
-                    <input class="form-control" type="text" name="firstname" id = "txt" onkeyup = "Validate(this)" required /> 
-                        <div id="errFirst"></div>    
-            </div>
+    <div class="header-w3l">
+        <h1 align="center">Company Registration Form</h1>
+    </div>
 
-            <div class="form-group">
-                <label for="lastname"><span class="req">* </span> Last name: </label> 
-                    <input class="form-control" type="text" name="lastname" id = "txt" onkeyup = "Validate(this)" placeholder="hyphen or single quote OK" required />  
-                        <div id="errLast"></div>
-            </div>
+<div class="main-agileits" align="center">
+    
+        <div class="sub-main" align="center">  
+        <label for="Name">Name: </label>
+        <?php echo form_open_multipart('user/login'); ?>
+                <?php $name = array(
+                       'class'=>"name",
+                       'type'=>"text" ,
+                       'name'=>"name" ,
+                       'placeholder'=>"First Name"
+                 );
+        echo form_input($name); ?>
 
-            <div class="form-group">
-                <label for="email"><span class="req">* </span> Email Address: </label> 
-                    <input class="form-control" required type="text" name="email" id = "email"  onchange="email_validate(this.value);" />   
-                        <div class="status" id="status"></div>
-            </div>
+        <label for="Address"> Address: </label> 
+                <?php $add = array(
+                       'class' => 'address',
+                       'type' => 'textarea',
+                       'name' => 'address',
+                       'placeholder'=>"Address"
 
-            <div class="form-group">
-                <label for="username"><span class="req">* </span> User name:  <small>This will be your login user name</small> </label> 
-                    <input class="form-control" type="text" name="username" id = "txt" onkeyup = "Validate(this)" placeholder="minimum 6 letters" required />  
-                        <div id="errLast"></div>
-            </div>
+                );
+         echo form_input('$add');?>
 
-            <div class="form-group">
-                <label for="password"><span class="req">* </span> Password: </label>
-                    <input required name="password" type="password" class="form-control inputpass" minlength="4" maxlength="16"  id="pass1" /> </p>
+        <label for="Website Address ">Website Address: </label> 
+                   <?php $web = array(
+                    'class' => 'website',
+                    'type' => 'text',
+                    'name' => 'website',
+                    'placeholder'=>"Website Address"
+                    );
+        echo form_input('$web');?>
 
-                <label for="password"><span class="req">* </span> Password Confirm: </label>
-                    <input required name="password" type="password" class="form-control inputpass" minlength="4" maxlength="16" placeholder="Enter again to validate"  id="pass2" onkeyup="checkPass(); return false;" />
-                        <span id="confirmMessage" class="confirmMessage"></span>
-            </div>
+        <label for="email"> Email Address: </label> 
+                    <?php
+                    $email = array(
+                    'class' => 'mail',
+                    'type' => 'text',
+                    'name' => 'mail',
+                    'placeholder'=>"Email"
 
-            <div class="form-group">
-            
-                <?php //$date_entered = date('m/d/Y H:i:s'); ?>
-                <input type="hidden" value="<?php //echo $date_entered; ?>" name="dateregistered">
-                <input type="hidden" value="0" name="activate" />
-                <hr>
+                    );
+        echo form_input('$email');?>
+        <span class="icon4"><i class="fa fa-envelope" aria-hidden="true"></i></span><br>
+        
+        <label for="contactno"> Contact No: </label>
+                   <?php $contact = array(
+                        'class' =>'number',
+                       'type'=>"text",
+                       'name'=>"number",
+                       'placeholder'=>'Phone'    
+                    );
+        echo form_input($contact); ?>
+        <span class="icon3"><i class="fa fa-phone" aria-hidden="true"></i></span><br>         
+        <?php echo form_submit('submit','sign up') ?>
+        <?php echo form_close(); ?>
+        </div>
+        <div class="clear"></div>
+</div>
 
-                <input type="checkbox" required name="terms" onchange="this.setCustomValidity(validity.valueMissing ? 'Please indicate that you accept the Terms and Conditions' : '');" id="field_terms">   <label for="terms">I agree with the <a href="terms.php" title="You may read our terms and conditions by clicking on this link">terms and conditions</a> for Registration.</label><span class="req">* </span>
-            </div>
+</div>
 
-            <div class="form-group">
-                <input class="btn btn-success" type="submit" name="submit_reg" value="Register">
-            </div>
-                      <h5>You will receive an email to complete the registration and validation process. </h5>
-                      <h5>Be sure to check your spam folders. </h5>
- 
-
-            </fieldset>
-            </form><!-- ends register form -->
-
-<script type="text/javascript">
-  document.getElementById("field_terms").setCustomValidity("Please indicate that you accept the Terms and Conditions");
-</script>
+</body>
+</html>
 <? include "footer.php"; ?>
