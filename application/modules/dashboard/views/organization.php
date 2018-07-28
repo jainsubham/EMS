4633095ee64fcc595662095853c2b1a3bb8f9be2
Narender Singh <123justdoitman@gmail.com>
2018-07-28 12:32:16 +0530

    initialization of organization structure module

--- application/modules/dashboard/views/organization.php
+++ application/modules/dashboard/views/organization.php
@@ -1,1 +1,168 @@
 <?php include('adminpannel.php') ;?>
+
+<?= link_tag('assets/css/treant.css') ?>
+<?= link_tag('assets/css/org-struct.css') ?>
+
+ <div class="main-panel">
+      <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
+        <div class="container-fluid">
+          <div class="navbar-wrapper">
+            <a class="navbar-brand" href="#">Organization Structure</a>
+          </div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+          </button>
+          
+        </div>
+      </nav>
+      <!-- End Navbar -->
+      <div class="content">
+        <div class="container-fluid"> 
+          <div class="col-lg-12">
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="card">
+                  <div class="card-header card-header-info">
+                   <h4 class="card-title">Graphical presentation of structure of your Organization </h4>
+               	  <!-- <p class="card-category">Complete your profile</p> -->
+                  </div>
+                <div class="card-body">
+
+		    		<div class="chart" id="org-struct"></div>
+		    	</div>
+			</div>
+		</div>
+	</div>
+	</div>
+	</div>
+	</div>
+</div>
+
+		    <script src="<?= base_url('assets/js/raphael.js') ?>"></script>
+		    <script src="<?= base_url('assets/js/treant.js') ?>"></script>
+    
+
+<script type="text/javascript">
+	var config = {
+        container: "#org-struct",
+        
+        connectors: {
+            type: 'step'
+        },
+        node: {
+            HTMLclass: 'nodeExample1'
+        }
+    },
+    c1 = {
+        text: {
+            name: "Mark Hill",
+            title: "Chief executive officer",
+            contact: "Tel: 01 213 123 134",
+        },
+        image: "../headshots/2.jpg"
+    },
+
+    cto = {
+        parent: c1,
+        text:{
+            name: "Joe Linux",
+            title: "Chief Technology Officer",
+        },
+        stackChildren: true,
+        image: "../headshots/1.jpg"
+    },
+    cbo = {
+        parent: c1,
+        stackChildren: true,
+        text:{
+            name: "Linda May",
+            title: "Chief Business Officer",
+        },
+        image: "../headshots/5.jpg"
+    },
+    cdo = {
+        parent: c1,
+        text:{
+            name: "John Green",
+            title: "Chief accounting officer",
+            contact: "Tel: 01 213 123 134",
+        },
+        image: "../headshots/6.jpg"
+    },
+    cio = {
+        parent: cto,
+        text:{
+            name: "Ron Blomquist",
+            title: "Chief Information Security Officer"
+        },
+        image: "../headshots/8.jpg"
+    },
+    ciso = {
+        parent: cto,
+        text:{
+            name: "Michael Rubin",
+            title: "Chief Innovation Officer",
+            contact: {val: "we@aregreat.com", href: "mailto:we@aregreat.com"}
+        },
+        image: "../headshots/9.jpg"
+    },
+    cio2 = {
+        parent: cdo,
+        text:{
+            name: "Erica Reel",
+            title: "Chief Customer Officer"
+        },
+        link: {
+            href: "http://www.google.com"
+        },
+        image: "../headshots/10.jpg"
+    },
+    ciso2 = {
+        parent: cbo,
+        text:{
+            name: "Alice Lopez",
+            title: "Chief Communications Officer"
+        },
+        image: "../headshots/7.jpg"
+    },
+    ciso3 = {
+        parent: cbo,
+        text:{
+            name: "Mary Johnson",
+            title: "Chief Brand Officer"
+        },
+        image: "../headshots/4.jpg"
+    },
+    ciso4 = {
+        parent: cbo,
+        text:{
+            name: "Kirk Douglas",
+            title: "Chief Business Development Officer"
+        },
+        image: "../headshots/11.jpg"
+    }
+
+    chart_config = [
+        config,
+        c1,
+        cto,
+        cbo,
+        cdo,
+        cio,
+        ciso,
+        cio2,
+        ciso2,
+        ciso3,
+        ciso4
+    ];
+
+	new Treant( chart_config );
+
+</script>
+
+</body>
+</html>
